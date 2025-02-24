e7295f1dcd22a7809e1845c7812b5fff9bf57d82
chyj0 <1254181605@qq.com>
2025-02-24 11:38:50 +0800

    修改名字

--- Erp-Blacked/src/main/java/com/erp/E02/Exception/UsernameAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.erp.E02.Exception;
-
-public class UsernameAlreadyExistsException extends RuntimeException {
-    public UsernameAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- Erp-Blacked/src/main/java/com/erp/E02/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.erp.E02.repository;
-
-import com.erp.E02.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
